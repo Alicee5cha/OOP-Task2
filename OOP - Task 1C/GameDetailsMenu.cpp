aed3d7c7d412b1c967fa0869572ba6ef19656f9a
Dallion3K <35102142+Dallion3K@users.noreply.github.com>
2020-04-01 13:02:19 +0100

    reeeeeeee

--- OOP - Task 1C/GameDetailsMenu.cpp
+++ OOP - Task 1C/GameDetailsMenu.cpp
@@ -19,6 +19,15 @@ void GameDetailsMenu::OutputOptions()
 			if (cUserLib[i]->getGame() == currentGame)
 			{
 				cout << "Game purchased on: " << cUserLib[i]->purchasedDate() << endl;
+				Option('P',"Play game!");
+				
+				if (cUserLib[i]->getTime() < 60)
+					printf("Total Time: %.0f", cUserLib[i]->getTime());
+				else if (cUserLib[i]->getTime() < 300)
+					printf("Total Time: %.1f", ((float)cUserLib[i]->getTime() / 60.0f));
+				else
+					printf("Total Time: %.0f", (cUserLib[i]->getTime()/60));
+
 				break;
 			}
 		}
@@ -43,24 +52,26 @@ bool GameDetailsMenu::HandleChoice(char choice)
 		if (app->IsUserLoggedIn())
 		{
 			vector<LibraryItem*> cUserLib = *(app->GetCurrentUser()->getLibrary());
-			int i;
-			for (i = 0; i < cUserLib.size(); i++)
+			
+			for (int i = 0; i < cUserLib.size(); i++)
 			{
 				if (cUserLib[i]->getGame() == currentGame)
 				{
+					//Play game
+					cUserLib[i]->addTime();
 					break;
 				}
-
-			}
-			if (cUserLib.size() == i)
-			{
-				//Purchase game
-				if (app->GetCurrentUser()->MinusCredits(currentGame->GetCost()))
+				if (cUserLib.size() == i)
 				{
-					app->GetCurrentUser()->getLibrary()->push_back(new LibraryItem(Utils::getCurrentDate(), currentGame, 0));
-					return true;
+					//Purchase game
+					if (app->GetCurrentUser()->MinusCredits(currentGame->GetCost()))
+					{
+						app->GetCurrentUser()->getLibrary()->push_back(new LibraryItem(Utils::getCurrentDate(), currentGame, 0));
+						return true;
+					}
 				}
 			}
+			
 		}
 	}
 	return false;
